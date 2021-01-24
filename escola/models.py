53b44fda97a987f6632ddea2b69e20559b77c21f
rogeriodelphi <rogeriodelphi@gmail.com>
2021-01-24 20:34:24 -0300

    Criando o models para matricula dos Alunos

--- escola/models.py
+++ escola/models.py
@@ -15,7 +15,7 @@ class Curso(models.Model):
     NIVEL = (
         ('B', 'Básico'),
         ('I', 'Intermediário'),
-        ('A', 'Avançado'),
+        ('A', 'Avançado')
     )
     codigo_curso = models.CharField(max_length=10)
     descricao = models.CharField(max_length=100)
@@ -23,3 +23,16 @@ class Curso(models.Model):
 
     def __str__(self):
         return self.descricao
+
+
+class Matricula(models.Model):
+    PERIODO = (
+        ('M', 'Matutino'),
+        ('V', 'Vespertino'),
+        ('N', 'Noturno')
+    )
+
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    periodo = models.CharField(max_length=1, choices=PERIODO, blank=False, null=False, default='M')
+
