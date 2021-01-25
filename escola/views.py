60504670691953448b986807b3cbe7846d888df3
rogeriodelphi <rogeriodelphi@gmail.com>
2021-01-24 21:46:36 -0300

    Criando uma ListAPIView

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
+
 from escola.models import Aluno, Curso, Matricula
-from .serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer
+from .serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer
+
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os aluno e alunas"""
@@ -18,3 +20,11 @@ class MatriculasViewSet(viewsets.ModelViewSet):
     """Exibindo todas as matrículas"""
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
+
+
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Listando as mat´riculas de um aluno ou aluna"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from escola.views import AlunosViewSet, CursosViewSet, MatriculasViewSet
+from escola.views import AlunosViewSet, CursosViewSet, MatriculasViewSet, ListaMatriculasAluno
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('Alunos', AlunosViewSet, basename='Alunos')
-router.register('Cursos', CursosViewSet, basename='Cursos')
-router.register('Matriculas', MatriculasViewSet, basename='Matriculas')
+router.register('alunos', AlunosViewSet, basename='Alunos')
+router.register('cursos', CursosViewSet, basename='Cursos')
+router.register('matriculas', MatriculasViewSet, basename='Matriculas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('alunos/<int:pk>/matriculas/', ListaMatriculasAluno.as_view()),
 ]
